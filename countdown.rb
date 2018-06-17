76ce665332f2752eaf296959f49d9ab8b1ad4918
Edsel Caprice <caprice@mail.usf.edu>
2018-06-17 16:00:31 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,14 @@
 #write your code here
 
 def countdown(number)
-  while number >0
+  while number > 0
 puts "#{number} SECOND(S)!"
 number-=1
 puts "HAPPY NEW YEAR!" if number == 0
   end
 end
+
+
 def countdown_with_sleep(number)
 while number >0
 puts "#{number} SECOND(S)!"
